fb813986c84cedb42c598f3734eb43cccc32447a
Myles Worth <myles.worth@icloud.com>
2022-02-08 15:39:19 +0000

    myscript.py

---
+++ myscript.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 15:30:53 2022
+
+@author: myles
+"""
+
+# myscript.py
+
+def double(thing):
+    """Doubles the thing"""
+    return thing + thing
+
+print(double(2))
+print(double([1, 2]))
+print(double("foo"))
